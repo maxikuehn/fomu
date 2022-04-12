ac5feacb8c64533c628aa70e4bc822e647be4c32
Maxi <maxi.j.kuehn@web.de>
2022-04-12 21:08:45 +0200

    redirect request to client

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express() // create express app
 app.use(express.static(path.join(__dirname, "..", "dist"))) // set static folder
 app.use(express.static("public"))
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
+})
+
 // start express server on port 3000
 app.listen(3000, () => {
   console.log("server started on port 3000")
