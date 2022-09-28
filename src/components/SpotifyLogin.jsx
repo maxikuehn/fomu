5c10489ed1f7f92bf992a3d5741f63f930f961a2
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2022-09-28 13:58:12 +0200

    JSON.parse Error handling

--- src/components/SpotifyLogin.jsx
+++ src/components/SpotifyLogin.jsx
@@ -1,13 +1,15 @@
 import { Button } from "antd"
 import { requestUserAuthorization } from "../services/Spotify"
 
-const REDIRECT_BASE = JSON.parse(import.meta.env.VITE_SP_REDIRECT_BASE)
+const REDIRECT_BASE = JSON.parse(import.meta.env.VITE_SP_REDIRECT_BASE ?? "[]")
 
 const SpotifyLogin = () => {
   const handleClick = () => {
     const href = window.location.href
     if (Array.isArray(REDIRECT_BASE) && REDIRECT_BASE.includes(href))
       requestUserAuthorization(href)
+    else
+      console.error("Redirect base not valid.", REDIRECT_BASE)
   }
 
   return (
