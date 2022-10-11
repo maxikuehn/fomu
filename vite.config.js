238653f3e157fa9dbd4ab1133a07ffcae9843855
kuehnbleiben <Gimajoku78!>
2022-10-11 11:32:51 +0200

    update packages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vitePluginImp from "vite-plugin-imp"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000 },
   plugins: [
     react(),
     vitePluginImp({
