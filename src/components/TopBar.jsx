7faacb572a7d84884bb99df29dcb1a2263e07b79
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2022-10-21 00:08:32 +0200

    weekly modal

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,64 +1,74 @@
 import { DownOutlined, LeftOutlined } from "@ant-design/icons"
-import { Avatar, Button, Dropdown, Menu } from "antd"
+import { Avatar, Button, Dropdown, Menu, Modal } from "antd"
 import Text from "antd/lib/typography/Text"
 import { useState } from "react"
 import { useRecoilState, useRecoilValue } from "recoil"
 import { appState, currentUserState, resetStates } from "../recoil"
 import { EAppState } from "../types"
-
-const ProfileMenu = () => {
-  const handleLogout = () => {
-    resetStates()
-  }
-  return (
-    <Menu
-      className="rounded-lg"
-      items={[{ label: "Logout", onClick: handleLogout, key: "logout2" }]}
-    />
-  )
-}
+import WeeklyDialog from "./WeeklyDialog"
 
 const TopBar = () => {
   const currentUserProfile = useRecoilValue(currentUserState)
   const [app, setApp] = useRecoilState(appState)
   const [open, setOpen] = useState(0)
+  const [openWeekly, setOpenWeekly] = useState(false)
 
   if (!currentUserProfile) return <div />
 
+  const ProfileMenu = () => (
+    <Menu
+      className="rounded-lg"
+      items={[
+        {
+          label: "Mix der Woche",
+          onClick: () => setOpenWeekly(true),
+          key: "weekly",
+        },
+        { label: "Logout", onClick: resetStates, key: "logout2" },
+      ]}
+    />
+  )
+
   return (
-    <div
-      id="topbar"
-      className="border-green-500 border- p-4 w-full flex justify-between items-center z-10"
-    >
-      {app === EAppState.Player && (
-        <Button
-          icon={<LeftOutlined />}
-          onClick={() => setApp(EAppState.Configuration)}
-        >
-          Einstellungen
-        </Button>
-      )}
-      <div />
-      <Dropdown
-        overlay={<ProfileMenu />}
-        onOpenChange={setOpen}
-        trigger="click"
-        placement="bottomRight"
-        className="bg-primary-400 rounded-full select-none cursor-pointer"
+    <>
+      <WeeklyDialog
+        open={openWeekly}
+        handleClose={() => setOpenWeekly(false)}
+      />
+      <div
+        id="topbar"
+        className="border-green-500 border- p-4 w-full flex justify-between items-center z-10"
       >
-        <div>
-          <Avatar
-            size={45}
-            src={currentUserProfile.images[0].url}
-            alt="Spotify Account Profile Picture"
-          />
-          <Text strong underline className="p-2">
-            {currentUserProfile.display_name}
-          </Text>
-          <DownOutlined size={"lg"} rotate={open ? 180 : 0} className="p-2" />
-        </div>
-      </Dropdown>
-    </div>
+        {app === EAppState.Player && (
+          <Button
+            icon={<LeftOutlined />}
+            onClick={() => setApp(EAppState.Configuration)}
+          >
+            Einstellungen
+          </Button>
+        )}
+        <div />
+        <Dropdown
+          overlay={<ProfileMenu />}
+          onOpenChange={setOpen}
+          trigger="click"
+          placement="bottomRight"
+          className="bg-primary-400 rounded-full select-none cursor-pointer"
+        >
+          <div>
+            <Avatar
+              size={45}
+              src={currentUserProfile.images[0].url}
+              alt="Spotify Account Profile Picture"
+            />
+            <Text strong underline className="p-2">
+              {currentUserProfile.display_name}
+            </Text>
+            <DownOutlined size={"lg"} rotate={open ? 180 : 0} className="p-2" />
+          </div>
+        </Dropdown>
+      </div>
+    </>
   )
 }
 export default TopBar
---
+++ src/components/WeeklyDialog.jsx
@@ -0,0 +1,111 @@
+import { Modal, Select, Typography } from "antd"
+import React, { useState, useEffect, memo } from "react"
+import { useRecoilValue } from "recoil"
+import api from "../api"
+import { useNotification } from "../Hooks/Notification"
+import {
+  currentUserOwnedPlaylistState,
+  currentUserPlaylistsState,
+} from "../recoil"
+const { OptGroup } = Select
+const { Text } = Typography
+
+const priorityTitles = [
+  ["Dein Mix der Woche", "Discover Weekly", "Descrubimeiento semanal"],
+  ["Release Radar"],
+]
+
+const WeeklyDialog = ({ open, handleClose }) => {
+  const [confirmLoading, setConfirmLoading] = useState(false)
+  const playlists = useRecoilValue(currentUserPlaylistsState)
+  const userPlaylists = useRecoilValue(currentUserOwnedPlaylistState)
+  const [playlistRecommended, setPlaylistRecommended] = useState([])
+  const [inputSelect, setInputSelect] = useState([])
+  const [outputSelect, setOutputSelect] = useState("")
+
+  useEffect(() => {
+    if (playlists.length === 0) return
+    setPlaylistRecommended(() => {
+      const _recommended = []
+      priorityTitles.forEach((title) => {
+        const playlist = playlists.find(
+          (p) => title.includes(p.name) && p.owner.id === "spotify"
+        )
+        if (playlist !== undefined) _recommended.push(playlist)
+      })
+      setInputSelect([_recommended[0]?.id ?? playlists[0]?.id])
+      return _recommended
+    })
+  }, [])
+
+  const handleOk = async () => {
+    if (inputSelect.length === 0 || outputSelect === "") return
+
+    setConfirmLoading(true)
+    try {
+      await api.weekly.register({ inputSelect, outputSelect })
+      handleClose()
+      useNotification({ content: "Für weekly registriert" })
+    } catch (_) {}
+    setConfirmLoading(false)
+  }
+
+  const Option = ({ id, name, owner }) => (
+    <Select.Option value={id} key={id} label={name}>
+      <div className=" flex justify-between absolute w-[calc(100%_-_55px)]">
+        <Text>{name}</Text>
+        <Text keyboard>{owner.display_name}</Text>
+      </div>
+    </Select.Option>
+  )
+
+  return (
+    <Modal
+      title="Mix der Woche"
+      open={open}
+      onOk={handleOk}
+      confirmLoading={confirmLoading}
+      onCancel={handleClose}
+      okButtonProps={{
+        disabled: inputSelect.length === 0 || outputSelect === "",
+      }}
+    >
+      <Text>
+        Wähle alle Playlists aus, die wöchentlich gespeichert werden sollen
+      </Text>
+      <Select
+        className="w-full"
+        value={inputSelect}
+        onChange={setInputSelect}
+        mode="multiple"
+        optionLabelProp="label"
+      >
+        {playlistRecommended.length > 0 ? (
+          <>
+            <OptGroup label="empfohlen">
+              {playlistRecommended.map(Option)}
+            </OptGroup>
+            <OptGroup label="weitere">
+              {playlists
+                .filter((playlist) => !playlistRecommended.includes(playlist))
+                .map(Option)}
+            </OptGroup>
+          </>
+        ) : (
+          playlists.map(Option)
+        )}
+      </Select>
+      <div className="h-6" />
+      <Text level={5}>Neue Tracks werden hier gespeichert</Text>
+      <Select
+        className="w-full"
+        value={outputSelect}
+        onChange={setOutputSelect}
+        optionLabelProp="label"
+      >
+        {userPlaylists.map(Option)}
+      </Select>
+    </Modal>
+  )
+}
+export default memo(WeeklyDialog, (p, n) => p.open === n.open)
