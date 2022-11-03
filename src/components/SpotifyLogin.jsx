4686a6aeccfae254204970157a7847e974107ceb
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2022-11-03 15:54:04 +0100

    font usage

--- src/components/SpotifyLogin.jsx
+++ src/components/SpotifyLogin.jsx
@@ -10,10 +10,10 @@ const SpotifyLogin = () => {
         id="login-button"
         onClick={handleClick}
         className="bg-white transition ease-[cubic-bezier(0.7,0,0.32,1.7)] duration-500 rounded-full
-         hover:shadow-spotify-green hover:shadow-[inset_0_0_0_10px,0px_10px_70px_-5px] p-8 cursor-pointer flex items-center space-x-4 "
+        hover:shadow-spotify-green hover:shadow-[inset_0_0_0_10px,0px_10px_70px_-5px] p-8 cursor-pointer flex items-center space-x-4"
       >
         <img src={SpotifyIcon} alt="Spotify Logo" className="h-20" />
-        <span className="text-black font-semibold text-4xl">
+        <span className="text-black font-metro font-medium text-4xl">
           Mit Spotify verbinden
         </span>
       </div>
