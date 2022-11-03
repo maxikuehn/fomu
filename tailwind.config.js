ca0497eeaf3007cd648d532a9609ef1834afa1ec
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2022-11-03 15:50:52 +0100

    added Metropolis to Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        metro: ["Metropolis", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: {
           0: "#156DC4",
