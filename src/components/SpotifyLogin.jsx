3d59ec84ddf397cf2385ca1badfe41704b5d91f2
Maximilian Kuehn <Maxij.Kuehn@bwedu.de>
2022-11-09 16:07:22 +0100

    login button select fix

--- src/components/SpotifyLogin.jsx
+++ src/components/SpotifyLogin.jsx
@@ -9,10 +9,15 @@ const SpotifyLogin = () => {
       <div
         id="login-button"
         onClick={handleClick}
-        className="flex cursor-pointer items-center space-x-4 rounded-full bg-white p-8 transition duration-500
-        ease-[cubic-bezier(0.7,0,0.32,1.7)] hover:shadow-[inset_0_0_0_10px,0px_10px_70px_-5px] hover:shadow-spotify-green"
+        className="flex cursor-pointer select-none items-center space-x-4 rounded-full bg-white p-8 transition
+        duration-500 ease-[cubic-bezier(0.7,0,0.32,1.7)] hover:shadow-[inset_0_0_0_10px,0px_10px_70px_-5px] hover:shadow-spotify-green"
       >
-        <img src={SpotifyIcon} alt="Spotify Logo" className="h-20" />
+        <img
+          src={SpotifyIcon}
+          alt="Spotify Logo"
+          className="h-20"
+          draggable={false}
+        />
         <span className="font-metro text-4xl font-medium text-black">
           Mit Spotify verbinden
         </span>
