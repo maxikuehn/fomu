893634390c2b854d31371fda72ce91e4f0e93603
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2023-01-20 13:24:30 +0100

    back to netlify functions

--- src/api/lib/token.ts
+++ src/api/lib/token.ts
@@ -1,7 +1,7 @@
 import { getRecoil, setRecoil } from "recoil-nexus"
-import { currentUserState, spotifyAuthState } from "../../recoil"
+import { spotifyAuthState } from "../../recoil"
 import scopes from "../../services/Scopes"
-import firebaseFetcher from "../firebaseFetcher.js"
+import netlifyFetcher from "../netlifyFetcher"
 
 const BASE_URL = window.location.origin
 const REDIRECT_URI = BASE_URL + "/callback"
@@ -9,10 +9,10 @@ const CLIENT_ID = import.meta.env.VITE_SP_CLIENT_ID
 
 export const refresh = async (failedRequest: any) => {
   console.log("refreshing token..")
-  const user_id = getRecoil(currentUserState)?.id
-  if (!user_id) return
+  const refresh_token = getRecoil(spotifyAuthState)?.refresh_token
+  if (!refresh_token) return
 
-  return firebaseFetcher.post("refreshToken", { user_id }).then((resp) => {
+  return netlifyFetcher.post("refresh", { refresh_token }).then((resp) => {
     setRecoil(
       spotifyAuthState,
       Object.assign({}, getRecoil(spotifyAuthState), resp.data)
@@ -21,6 +21,7 @@ export const refresh = async (failedRequest: any) => {
       failedRequest.response.config.headers[
         "Authorization"
       ] = `Bearer ${resp.data.access_token}`
+      console.log("failedRequest", failedRequest.response)
     }
     return Promise.resolve()
   })
@@ -29,8 +30,8 @@ export const refresh = async (failedRequest: any) => {
 export const get = async (code: string) => {
   if (!code || code === "") return
   const code_verifier = window.localStorage.getItem("code_verifier")
-  return firebaseFetcher
-    .post("getToken", {
+  return netlifyFetcher
+    .post("token", {
       href: REDIRECT_URI,
       code,
       code_verifier,
