3c194c7c93d1fa8acb5ccc56cc28264dedf1d574
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2023-03-22 10:49:52 +0100

    update email

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -45,6 +45,7 @@ const handler = async function (event) {
         where: { id: sp_user.data.id },
         update: {
           refresh_token: resp.data.refresh_token,
+          email: sp_user.data.email,
         },
         create: {
           id: sp_user.data.id,
