7411249402696fcdccae0bc110c0599cd233e8b1
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2023-05-19 16:13:07 +0200

    hard code public

--- netlify/functions/public.js
+++ netlify/functions/public.js
@@ -1,7 +1,7 @@
 const handler = async function () {
   return {
     statusCode: 200,
-    body: process.env.PUBLIC_KEY,
+    body: "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAwrQsE0/qUe6e4OJCNEA6Bro7xOIh+dE/U/eMe+FNplG4FsPmtVEmtX1tfuS7AyWhrsK3uyf9bblHgnAX2OyK8lVEOX+yZWyCbIuMxgzcB7gBejjaTMfwNtMryYZNMSyJKWKKAPxsiLEdYqVajk/GLnubODH+Nq24cBgAnGgLhX6FUt6BOBEuYSCWC9GhIJoCbWFHQ3gsSqipTcafEu/HSegrspmK8dy7sV90SZNQpHxau6KE0AQJbQCXBwA0ZlHAKh1gljaGhsipMxtr9KdT/Ss1qDxp2K3ICgGCp9kke1tzc3Hc+bs3dUczIFyh+dRl5sBoh4xMPASJdjtlVhGGtyze8ebnUMcrJnLm67YHuSQd2As4poxkWHVNnIHs33g2TdRVqg0Th4a3uP9Jc0m6PuUbFOAmLdHhus6+DMaBtDxJrnYiU5zZVnCJau/NuTXSEtRtL6dPbThrQv3bS5XdiU+mEUnGOzEGXHrkVNHWW552nBk+xDFatibZaAIlAhqNouroEmaz5Vhq7eTrFblBVOH8E+t1yspqrFG6HDr1uMVCgK712in+4Gs/+Gsp41IM7HwK44GsF0SQJb8t81WXye/6XuSrZxRgQubyK477jtKgUgHzQX2JmAfrLjrXS2FAME+kIZo269/zll1ic2RtD7BaTYnsHY0qWXvGT+V7t/8CAwEAAQ==\n-----END PUBLIC KEY-----",
   }
 }
 
