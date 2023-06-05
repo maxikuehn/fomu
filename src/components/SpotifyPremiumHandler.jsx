e5486f73f92db8978095d85ab53e95916f43d7ac
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2023-06-05 21:25:28 +0200

    spotifypremiumhandler responsive

--- src/components/SpotifyPremiumHandler.jsx
+++ src/components/SpotifyPremiumHandler.jsx
@@ -1,34 +1,37 @@
-import { Button, Image } from "antd"
+import { Button } from "antd"
 import { logout } from "../recoil"
 
 const SpotifyPremiumHandler = () => {
   return (
-    <div className="absolute z-50 flex h-full w-full flex-col items-center justify-center bg-background">
-      <div className="flex flex-col space-y-5 p-4">
-        <div className="flex items-start">
-          <Image
-            src="srcassetsimagesSpotify_Logo_RGB_Green.png"
-            preview={false}
-            height={55}
-            className="mt-[7px] hover:cursor-pointer"
+    <div className="absolute flex h-full w-full flex-col items-center justify-center bg-background">
+      <div className="flex flex-col p-4">
+        <div className="flex md:items-center items-start flex-col md:flex-row">
+          <img
+            src="src/assets/images/Spotify_Logo_RGB_Green.png"
+            alt="Spotify Logo"
+            className="cursor-pointer h-14 mb-1"
             onClick={() => window.open("https://www.spotify.com/premium")}
+            onKeyDown={() => window.open("https://www.spotify.com/premium")}
           />
-          <div className="ml-2 font-metro text-4xl font-medium leading-loose ">
+          <span className="ml-2 font-metro text-4xl font-medium leading-loose">
             Premium notwendig
+          </span>
+        </div>
+        <div className="max-w-sm flex flex-col gap-4 justify-center">
+          <div className="text-lg">
+            Leider ist ein Spotify Premium Account notwendig um die Funktionen
+            von{" "}
+            <span className="whitespace-nowrap font-metro font-medium tracking-[0.3em]">
+              {"FOMU"}
+            </span>{" "}
+            nutzen zu können. Bitte melde dich mit einem Spotify-Premium Account
+            an.
           </div>
-        </div>
-        <div className="w-96 text-lg">
-          Leider ist ein Spotify Premium Account notwendig um die Funktionen von{" "}
-          <span className="whitespace-nowrap font-metro font-medium tracking-[0.25em] ">
-            {"FOMU"}
-          </span>{" "}
-          nutzen zu können. Bitte melde dich mit einem Premium Account an.
-        </div>
-        <div>
-          <Button onClick={logout}>abmelden</Button>
+          <Button onClick={logout} type="default">
+            Abmelden
+          </Button>
         </div>
       </div>
-      <div className="h-52" />
     </div>
   )
 }
