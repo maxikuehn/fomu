f75c1f1844cd80a135d0a3a240ace732451eeb43
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2023-06-05 21:57:58 +0200

    loading logout option

--- src/views/LoadingPage.jsx
+++ src/views/LoadingPage.jsx
@@ -1,15 +1,16 @@
 import { LoadingOutlined } from "@ant-design/icons"
-import { Spin } from "antd"
-import { useEffect } from "react"
+import { Button, Spin } from "antd"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useSetRecoilState } from "recoil"
 import api from "../api"
-import { appLoadingState, spotifyAuthState } from "../recoil"
+import { appLoadingState, logout, spotifyAuthState } from "../recoil"
 
 const LoadingPage = () => {
   const navigate = useNavigate()
   const setAuthState = useSetRecoilState(spotifyAuthState)
   const setAppLoading = useSetRecoilState(appLoadingState)
+  const [showLogout, setShowLogout] = useState(false)
 
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get("code")
@@ -21,17 +22,30 @@ const LoadingPage = () => {
     })
   }, [])
 
+  useEffect(() => {
+    setShowLogout(false)
+    const timer = setTimeout(() => {
+      setShowLogout(true)
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div className="absolute top-0 flex h-screen w-screen items-center justify-center bg-background">
-      <Spin
-        indicator={
-          <LoadingOutlined
-            style={{
-              fontSize: 100,
-            }}
-          />
-        }
-      />
+    <div className="absolute inset-0 flex items-center justify-center bg-background">
+      <div className="flex flex-col gap-12">
+        <Spin
+          indicator={
+            <LoadingOutlined
+              style={{
+                fontSize: 100,
+              }}
+            />
+          }
+        />
+        <div className="h-8">
+          {showLogout && <Button onClick={logout}>Abmelden</Button>}
+        </div>
+      </div>
     </div>
   )
 }
